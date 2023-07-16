353a3a1a9823530672d97daf984ba9d491aaa495
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2023-07-16 23:06:00 +0530

    Create get inventoryApi to fetch all the details of donar who donated his blood

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const authMiddlewares = require("../middlewares/authMiddlewares");
 const {
   createInventoryController,
+  getInventoryController,
 } = require("../controllers/inventoryController");
 
 const router = express.Router();
 
 // routes
+
+// Create Inventory || POST
 router.post("/create-inventory", authMiddlewares, createInventoryController);
+
+// Get Blood Record || GET
+router.get("/get-inventory", authMiddlewares, getInventoryController);
 
 module.exports = router;
