ac077154471efd67efb67208d9254cf83be6bb5c
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2023-07-18 22:46:08 +0530

    Create redux component for statemanagement

--- client/src/components/shared/form/InputTypes.jsx
+++ client/src/components/shared/form/InputTypes.jsx
@@ -10,7 +10,7 @@ const InputTypes = ({
 }) => {
   return (
     <>
-      <div className="mb-3">
+      <div className="mb-1">
         <label htmlFor={labelFor} className="form-label">
           {labelText}
         </label>
